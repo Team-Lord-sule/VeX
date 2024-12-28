ff9b95b1497a1706c260f7c36507eed12fd4ccc0
Mr-Tarif-130 <mr.tarif130@gmail.com>
2024-12-28 10:29:14 +0600

    Create out.js

---
+++ scripts/cmds/out.js
@@ -0,0 +1,33 @@
+const { GoatWrapper } = require("fca-liane-utils");
+const axios = require("axios");
+const fs = require("fs-extra");
+const request = require("request");
+module.exports = {
+	config: {
+		name: "out",
+		aliases: ["l"],
+		version: "1.0",
+		author: "Sandy",
+		countDown: 5,
+		role: 2,
+		shortDescription: "bot will leave gc",
+		longDescription: "",
+		category: "admin",
+		guide: {
+			vi: "{pn} [tid,blank]",
+			en: "{pn} [tid,blank]"
+		}
+	},
+
+	onStart: async function ({ api,event,args, message }) {
+ var id;
+ if (!args.join(" ")) {
+ id = event.threadID;
+ } else {
+ id = parseInt(args.join(" "));
+ }
+ return api.sendMessage('bot leave the group', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
+		}
+	};
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
