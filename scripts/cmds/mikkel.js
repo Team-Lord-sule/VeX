5bbc66c8f506f2c94758f22563e865314082bc41
Mr-Tarif-130 <mr.tarif130@gmail.com>
2024-12-29 15:45:30 +0600

    Create mikkel.js

---
+++ scripts/cmds/mikkel.js
@@ -0,0 +1,57 @@
+const { GoatWrapper } = require("fca-liane-utils");
+const DIG = require("discord-image-generation");
+const fs = require("fs-extra");
+
+module.exports = {
+  config: {
+    name: "mikkel",
+    version: "1.0",
+    author: "𝐀𝐒𝐈𝐅 𝐱𝟔𝟗",
+    countDown: 1,
+    role: 0,
+    shortDescription: "Mikkelsen Image!",
+    longDescription: "",
+    category: "𝗙𝗨𝗡",
+    guide: "{pn} [mention|leave_blank]",
+    envConfig: {
+      deltaNext: 5
+    }
+  },
+
+  langs: {
+    vi: {
+      noTag: "Bạn phải tag người bạn muốn tát"
+    },
+    en: {
+      noTag: "You must tag the person you want to "
+    }
+  },
+
+  onStart: async function ({ event, message, usersData, args, getLang }) {
+    let mention = Object.keys(event.mentions)
+    let uid;
+
+    if (event.type == "message_reply") {
+      uid = event.messageReply.senderID
+    } else {
+      if (mention[0]) {
+        uid = mention[0]
+      } else {
+        console.log(" jsjsj")
+        uid = event.senderID
+      }
+    }
+
+    let url = await usersData.getAvatarUrl(uid)
+    let avt = await new DIG.Mikkelsen().getImage(url)
+
+    const pathSave = `${__dirname}/tmp/Mikkelsen.png`;
+    fs.writeFileSync(pathSave, Buffer.from(avt));
+    // Send the image as a reply to the command message
+    message.reply({
+      attachment: fs.createReadStream(pathSave)
+    }, () => fs.unlinkSync(pathSave));
+  }
+};
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
