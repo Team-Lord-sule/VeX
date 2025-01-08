14e6ddd91140548c229046ffdfadb1b34755beb8
MR-TARIF-YT-130 <mr.tarif130@gmail.com>
2025-01-08 10:55:09 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -82,7 +82,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "👍"){
+        if(event.reaction == "🤖"){
   if(event.senderID == api.getCurrentUserID()){if(event.userID == "100081491574719","61552422054139","61570905260990"){
     message.unsend(event.messageID)
 }else{
